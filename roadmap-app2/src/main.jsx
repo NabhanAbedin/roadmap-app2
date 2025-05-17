2147af879280071fc6fcc64739198b7eb6ab85b3
Nabhan <nabedi2006@gmail.com>
2025-05-16 20:50:18 -0400

    transfered all components

--- roadmap-app2/src/main.jsx
+++ roadmap-app2/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {ThemeProvider} from './components/settings_folder/ThemeContext.jsx';
+import App from './components/App.jsx';
+import {BrowserRouter} from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
 
-    <App />
-  
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+
+
+);
+
+
